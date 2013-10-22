25cc541ec92e133902a3feb394613d8ba01b22d8
pengdong17 <838317717@qq.com>
2013-10-22 17:50:37 +0800

    test

---
+++ statis.js
@@ -0,0 +1,3 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F7fb37cb72d7723dcb46e14756c6b23b6' type='text/javascript'%3E%3C/script%3E"));
+document.writeln("<script src=\'http:\/\/w.cnzz.com\/c.php?id=30020228\' language=\'JavaScript\' charset=\'gb2312\'><\/script>");
